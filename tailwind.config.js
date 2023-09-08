a16ae1157f8ec593bf0f0151783bf5fb08d86d6f
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-08 13:44:53 +0100

    added all files

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "purple": "hsl(259, 100%, 65%)",
+          "lightred": "hsl(0, 100%, 67%)"
+        },
+        neutral: {
+          "white": "hsl(0, 0%, 100%)",
+          "offwhite": "hsl(0, 0%, 94%)",
+          "lightgrey": "hsl(0, 0%, 86%)",
+          "smokeygrey": "hsl(0, 1%, 44%)",
+          "offblack": "hsl(0, 0%, 8%)"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
