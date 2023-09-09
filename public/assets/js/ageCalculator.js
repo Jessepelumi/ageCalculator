d6c46c93147f6b92427d82fb3c3dd341d2a01637
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-09 08:51:02 +0100

    added form structure

--- public/assets/js/ageCalculator.js
+++ public/assets/js/ageCalculator.js
@@ -2,3 +2,6 @@ const dayInput = document.getElementById("day");
 const monthInput = document.getElementById("month");
 const yearInput = document.getElementById("year");
 
+if (dayInput.textContent === "") {
+    
+}
